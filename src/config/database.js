81338e3a645bb696396019ee8b93435e23654b79
Victor.Breno <87786280+VictorBren0@users.noreply.github.com>
2023-02-16 14:25:50 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,10 @@ require('dotenv/config')
 
 module.exports = {
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'apiunigeek',
+  host: 'aws-sa-east-1.connect.psdb.cloud',
+  username: '09of6ovybz04gwukl332',
+  password: 'pscale_pw_COxPV8rBw1LfjxxCIXWsbm9j1FbVuZFXLsynNr5C7BR',
+  database: 'unigeek',
   port: '3306',
   seedStorage: 'sequelize',
   define: {
