50a72be0172b20e0cd83e7fba1a6c8600a187adf
Victor Breno <victor.bsr2000@gmail.com>
2023-03-21 17:52:38 -0300

    FIX: Resolvido erro de nome da tabela

--- src/database/migrations/20230218003722-create-category_events.js
+++ src/database/migrations/20230218003722-create-category_events.js
@@ -36,6 +36,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('category_subcategorys')
+    await queryInterface.dropTable('category_events')
   },
 }
